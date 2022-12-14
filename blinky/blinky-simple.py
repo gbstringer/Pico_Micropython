f646b0edd81ec25723c6cf9c2ead17b12941da5d
Gary Stringer <G.B.Stringer@exeter.ac.uk>
2022-12-14 18:41:04 +0000

    Add comments to blinky/blinky-simple.py

--- blinky/blinky-simple.py
+++ blinky/blinky-simple.py
@@ -1,8 +1,8 @@
-from machine import Pin
-import time
+from machine import Pin	# import the definitions for interacting with pins (GPn)
+import time				# load the 'time' library, which has lots of time-related functions
 
-led = Pin(1, Pin.OUT)
+led = Pin(1, Pin.OUT)	# define a pin for output
 
-while True:
-    led.toggle()
-    time.sleep_ms(200)	# 200 ms = 1/5 second
+while True:				# do this forever!
+    led.toggle()		# toggle the led pin
+    time.sleep_ms(200)	# pause for 200 ms = 1/5 second
